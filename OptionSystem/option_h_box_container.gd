92d055c407cd7d78af458e8ac13d43611d63e1f7
Thlipsi <121302871+Thlipsi@users.noreply.github.com>
2024-09-13 03:58:28 -0700

    tinkering
    dont use resource tree for paths, try just using booleans in a story manager

---
+++ OptionSystem/option_h_box_container.gd
@@ -0,0 +1,13 @@
+extends HBoxContainer
+
+
+
+func _on_option_1_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_option_2_pressed() -> void:
+	pass # Replace with function body.
+
+
+#bool, enum, resource, 
---
+++ OptionSystem/path_resource.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name Path
+
+@export var paths: Array[Path]
+
+var story_component: String
